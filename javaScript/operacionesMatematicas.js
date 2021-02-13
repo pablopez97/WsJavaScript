df054ce1ea61e2277f9fa50c7fb8075a9778ede9
Pablo <pablopezgarcia97@gmail.com>
2021-02-13 14:22:26 +0100

    Ejercicio operacion matematica

---
+++ javaScript/operacionesMatematicas.js
@@ -0,0 +1,21 @@
+
+
+function sumar(a,b){
+    var resultado = a + b;
+    return resultado;
+  }
+
+function restar(a,b){
+    var resultado = a - b;
+    return resultado;
+}
+
+function multiplicar(a,b){
+    var resultado = a * b;
+    return resultado;
+}
+
+function dividir(a,b){
+    var resultado = a / b;
+    return resultado;
+}
